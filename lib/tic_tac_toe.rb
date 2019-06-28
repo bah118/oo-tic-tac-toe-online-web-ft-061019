dc95f8900ee5b861ec2df04d441f6b10f1f4d809
BethAnn Hanlon <herbivore137@gmail.com>
2019-06-28 17:04:57 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,11 @@
+class TicTacToe
+  
+  WIN_COMBINATIONS = [["x", "x", "x", " ", " ", " ", " ", " ", " "], [" ", " ", " ", "x", "x", "x", " ", " ", " "]]
+  
+  
+  def initialize
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+  
+  
+end
